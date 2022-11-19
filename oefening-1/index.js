8fc93b4aa13cedab1ca0c86d619237b1bda5caad
NickFury <miaytac1@gmail.com>
2022-11-19 11:01:47 +0100

    oef1: validatie

--- oefening-1/index.js
+++ oefening-1/index.js
@@ -9,3 +9,39 @@ const postcode = vraag("Postcode");
 const gemeente = vraag("Gemeente");
 
 // valideren
+let ongeldig = "";
+
+if (voornaam === "") {
+  ongeldig = "Minstens één van de ingevulde velden is niet geldig!";
+} else if (voornaam.length > 44) {
+  ongeldig = "Kader is te breed. Gebruik afkortingen in de breedste stukken.";
+}
+
+if (familienaam === "") {
+  ongeldig = "Minstens één van de ingevulde velden is niet geldig!";
+} else if (familienaam.length > 44) {
+  ongeldig = "Kader is te breed. Gebruik afkortingen in de breedste stukken.";
+}
+
+if (straatenhuisnummer === "") {
+  ongeldig = "Minstens één van de ingevulde velden is niet geldig!";
+} else if (straatenhuisnummer.length > 44) {
+  ongeldig = "Kader is te breed. Gebruik afkortingen in de breedste stukken.";
+}
+
+if (isNaN(parseInt(postcode)) || postcode < 999 || postcode > 9990) {
+  ongeldig = "Minstens één van de ingevulde velden is niet geldig!";
+} else if (postcode.length > 44) {
+  ongeldig = "Kader is te breed. Gebruik afkortingen in de breedste stukken.";
+}
+
+if (gemeente === "") {
+  ongeldig = "Minstens één van de ingevulde velden is niet geldig!";
+} else if (gemeente.length > 44) {
+  ongeldig = "Kader is te breed. Gebruik afkortingen in de breedste stukken.";
+}
+
+if (ongeldig !== "") {
+  console.log(ongeldig);
+} else {
+}
